5c6fbd69cf0c09516ad9d924fefacae3089bc866
Denis Novitsky <den.novc@gmail.com>
2021-02-27 17:44:49 +0700

    Project initialization

---
+++ WhatToWatch/Application/AppDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  AppDelegate.swift
+//  WhatToWatch
+//
+//  Created by Denis Novitsky on 27.02.2021.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {}
---
+++ WhatToWatch/Application/SceneDelegate.swift
@@ -0,0 +1,10 @@
+//
+//  SceneDelegate.swift
+//  WhatToWatch
+//
+//  Created by Denis Novitsky on 27.02.2021.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {}
