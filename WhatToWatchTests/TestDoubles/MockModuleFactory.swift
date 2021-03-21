f8ed48fe2956fa01211c00049345ac54a93d6068
Denis Novitsky <den.novc@gmail.com>
2021-03-21 19:56:05 +0700

    Add tests for SearchCoordinator

---
+++ WhatToWatchTests/TestDoubles/MockModuleFactory.swift
@@ -0,0 +1,10 @@
+//
+//  MockModuleFactory.swift
+//  WhatToWatchTests
+//
+//  Created by Denis Novitsky on 21.03.2021.
+//
+
+@testable import WhatToWatch
+
+final class MockModuleFactory: ModuleFactoryProtocol {}
