8f0555c650d1258d42c674ce003cd2db6f663814
Denis Novitsky <den.novc@gmail.com>
2021-05-09 20:27:24 +0700

    Move API info from xcconfig to plist

--- WhatToWatch/Application/AppConfiguration.swift
+++ WhatToWatch/Application/AppConfiguration.swift
@@ -9,22 +9,30 @@ import Foundation
 
 final class AppConfiguration {
 
+    private let apiInfo: NSDictionary = {
+        guard let path = Bundle.main.path(forResource: "API-Info", ofType: "plist") else {
+            fatalError("Couldn't find file 'API-Info.plist'.")
+        }
+
+        return NSDictionary(contentsOfFile: path)!
+    }()
+
     private(set) lazy var apiKey: String = {
-        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "APIKey") as? String else {
+        guard let apiKey = apiInfo.object(forKey: "APIKey") as? String else {
             fatalError("APIKey must not be empty in plist")
         }
         return apiKey
     }()
 
     private(set) lazy var apiBaseURL: String = {
-        guard let apiBaseURL = Bundle.main.object(forInfoDictionaryKey: "APIBaseURL") as? String else {
+        guard let apiBaseURL = apiInfo.object(forKey: "APIBaseURL") as? String else {
             fatalError("APIBaseURL must not be empty in plist")
         }
         return apiBaseURL
     }()
 
     private(set) lazy var imageBaseURL: String = {
-        guard let imageBaseURL = Bundle.main.object(forInfoDictionaryKey: "ImageBaseURL") as? String else {
+        guard let imageBaseURL = apiInfo.object(forKey: "ImageBaseURL") as? String else {
             fatalError("ImageBaseURL must not be empty in plist")
         }
         return imageBaseURL
